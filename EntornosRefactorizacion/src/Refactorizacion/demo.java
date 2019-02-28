7c2110bffb587bfbd885e904ebefa5c6ee4e507f
Esther Lainez <esthertarazona81@gmail.com>
2019-02-28 23:18:38 +0100

    modifico

--- EntornosRefactorizacion/src/Refactorizacion/demo.java
+++ EntornosRefactorizacion/src/Refactorizacion/demo.java
@@ -1,22 +1,23 @@
 package Refactorizacion;
 
+import EjemploSinRefactorizar.GeneraPrimosNoRefactorizadoEjemplo;
+//importo al no funcionarme la clase GeneradorDePrimos
 public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
 			int primos[];
 			int limite = 125;
-			primos = GeneradorDePrimos.generarPrimos(limite);
+			primos = GeneraPrimosNoRefactorizadoEjemplo.generarPrimos(limite);
 			
 			System.out.println("Lista de números primos entre 0 y "+limite);
 			for (int i=0; i<primos.length; i++) {
 				System.out.println(primos[i]);
 			}
 			
-			}catch(NullPointerException e) {
-				System.out.println("esto no esta funcionando...");
-			}
+			//catch(NullPointerException e) {
+			//System.out.println("esto no esta funcionando...hice try catch");
+			
 	}
 
 }
